65b7cf3824801fe5978436b4a3963589d115ba6f
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2025-02-20 12:11:01 -0300

    DockerFile

--- Back-End/src/main/java/com/frete/mais/gerenciamento_de_entregas/cors/CorsConfig.java
+++ Back-End/src/main/java/com/frete/mais/gerenciamento_de_entregas/cors/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.frete.mais.gerenciamento_de_entregas.cors;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://gerenciamento-de-entregas.vercel.app")
+                .allowedOrigins("https://gerenciamento-de-entregas.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
